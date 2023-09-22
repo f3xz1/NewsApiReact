3cb1a9ee977ff74ae092a0433a793df1649175d7
f3xz1 <dgagnidzeh@gmail.com>
2023-09-22 19:56:55 +0400

    22.09.2023

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=5b540e74f4a648fe85b08038e322b971"
+    )
+      .then((response) => response.json())
+      .then((data) => setNews(data.articles));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>News</h1>
+      <ul>
+        {news.map((article, index) => (
+          <li key={index}>{article.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
